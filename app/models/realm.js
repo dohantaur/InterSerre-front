64122227ae95cfea8bd6d304366e339f0dd6f758
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-05-18 17:08:14 +0200

    begin management

--- app/models/realm.js
+++ app/models/realm.js
@@ -9,7 +9,7 @@ var realm = DS.Model.extend({
 realm.reopenClass({
   FIXTURES: [
     { id: 1, greenHouse: 1, sensors: [1, 2]},
-    { id: 1, greenHouse: 2, sensors: [3, 4]}
+    { id: 2, greenHouse: 2, sensors: [3, 4]}
   ]
 });
 
