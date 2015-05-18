3d7409a1c14280e58d29146394a332e852fc314b
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-05-18 16:39:33 +0200

    add models and fixtures

--- app/models/sensor.js
+++ app/models/sensor.js
@@ -1,8 +1,26 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  realm: DS.belongsTo('realm'),
+var sensor =  DS.Model.extend({
+  realm: DS.belongsTo('realm' , {async: true}),
   type: DS.attr('string'),
   name: DS.attr('string'),
-  value: DS.attr('number')
+  lastValue: DS.attr('number'),
+  values: []
 });
+
+sensor.reopenClass({
+  FIXTURES: [
+    { id: 1, realm: 1, type: 'float', name: 'temperature', lastValue: 0},
+    { id: 2, realm: 1, type: 'float', name: 'humidity', lastValue: 0},
+    { id: 3, realm: 2, type: 'float', name: 'temperature', lastValue: 0},
+    { id: 4, realm: 2, type: 'float', name: 'humidity', lastValue: 0},
+    { id: 5, realm: 2, type: 'float', name: 'testosterone', lastValue: 0},
+    { id: 6, realm: 2, type: 'float', name: 'decibel', lastValue: 0},
+    { id: 7, realm: 1, type: 'float', name: 'testosterone', lastValue: 0},
+    { id: 8, realm: 1, type: 'float', name: 'decibel', lastValue: 0},
+    { id: 9, realm: 2, type: 'float', name: 'connerie', lastValue: 0},
+    { id: 10, realm: 2, type: 'float', name: 'mere a julian', lastValue: 0}
+  ]
+});
+
+export default sensor
