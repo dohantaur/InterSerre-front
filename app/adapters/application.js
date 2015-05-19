8a62586b85a0302196c304721aa1373b2b8ce5e5
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-05-19 13:29:26 +0200

    serialize + adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
-var ApplicationAdapter = DS.FixtureAdapter.extend();
+var ApplicationAdapter = DS.RESTAdapter.extend();
 
 export default ApplicationAdapter;
--- app/models/sensor.js
+++ app/models/sensor.js
@@ -2,24 +2,24 @@ import DS from 'ember-data';
 
 var sensor =  DS.Model.extend({
   realm: DS.belongsTo('realm' , {async: true}),
-  type: DS.attr('string'),
   name: DS.attr('string'),
+  unit: DS.attr('string'),
   lastValue: DS.attr('number'),
   values: []
 });
 
 sensor.reopenClass({
   FIXTURES: [
-    { id: 1, realm: 1, type: 'float', name: 'temperature', lastValue: 0},
-    { id: 2, realm: 1, type: 'float', name: 'humidity', lastValue: 0},
-    { id: 3, realm: 2, type: 'float', name: 'temperature', lastValue: 0},
-    { id: 4, realm: 2, type: 'float', name: 'humidity', lastValue: 0},
-    { id: 5, realm: 2, type: 'float', name: 'testosterone', lastValue: 0},
-    { id: 6, realm: 2, type: 'float', name: 'decibel', lastValue: 0},
-    { id: 7, realm: 1, type: 'float', name: 'testosterone', lastValue: 0},
-    { id: 8, realm: 1, type: 'float', name: 'decibel', lastValue: 0},
-    { id: 9, realm: 2, type: 'float', name: 'connerie', lastValue: 0},
-    { id: 10, realm: 2, type: 'float', name: 'mere a julian', lastValue: 0}
+    { id: 1, realm: 1, unit: 'float', name: 'temperature', lastValue: 0},
+    { id: 2, realm: 1, unit: 'float', name: 'humidity', lastValue: 0},
+    { id: 3, realm: 2, unit: 'float', name: 'temperature', lastValue: 0},
+    { id: 4, realm: 2, unit: 'float', name: 'humidity', lastValue: 0},
+    { id: 5, realm: 2, unit: 'float', name: 'testosterone', lastValue: 0},
+    { id: 6, realm: 2, unit: 'float', name: 'decibel', lastValue: 0},
+    { id: 7, realm: 1, unit: 'float', name: 'testosterone', lastValue: 0},
+    { id: 8, realm: 1, unit: 'float', name: 'decibel', lastValue: 0},
+    { id: 9, realm: 2, unit: 'float', name: 'connerie', lastValue: 0},
+    { id: 10, realm: 2, unit: 'float', name: 'mere a julian', lastValue: 0}
   ]
 });
 
