a26a57c424a8fb76d300ac162c45837a2ce48844
ZeTomate <elsa.rousselbach@gmail.com>
2015-05-20 11:28:34 +0200

    fin design pop-up

---
+++ app/controllers/management/edit-all.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  listEtape: ["Jachère", "Semis", "Plants", "Fleurs", "Croissance", "Maturation", "Récolte" ]
+});
--- app/controllers/managment/edit-all.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-  init:function(){
-    consol.log("vbn,njb,n");
-  },
-  listEtape: ["Aucun", "Jachère", "Semis", "Plants", "Fleurs", "Croissance", "Maturation", "Récolte" ]
-});
