88446f379684e8cf978853b665d6b27c71e2776f
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-05-22 09:38:55 +0200

    actions

--- app/controllers/management/action.js
+++ app/controllers/management/action.js
@@ -1,4 +1,30 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actionValue: false,
+  actions: {
+    saveAction() {
+      var self = this;
+      console.log(this.get('actionValue'));
+      Ember.$.ajax('http://localhost:3000/api/actions',{
+        contentType: 'application/json',
+        dataType: "json",
+        method: 'POST',
+        success: function(s) {
+          console.log(s);
+          self.transitionToRoute('management');
+        },
+        error: function(s) {
+          console.log(s);
+          self.transitionToRoute('management');
+        },
+        data: JSON.stringify({sensorValue: this.get('actionValue')})
+      }).then(function(result) {
+        console.log('thenn')
+
+        }, function(failure) {
+          console.log(failure);
+        });
+    }
+  }
 });
