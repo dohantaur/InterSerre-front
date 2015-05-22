28e5eb62eeccdf13ec1218ef43a0f92653658a78
Aurelien Dohant <dohant.aurelien@gmail.com>
2015-05-22 11:22:23 +0200

    final

--- app/components/compo-index.js
+++ app/components/compo-index.js
@@ -1,11 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  image1:null,
-  DidInsertElement:function(){
-    this.set('image1' , Ember.$('#image1'));
-    Ember.run.later(this,function(){
 
-    })
+  actualClass: 'img1',
+
+  actualize: function() {
+    Ember.run.later(this,function(){
+      switch(this.get('actualClass')) {
+        case 'img1':
+          this.set('actualClass', 'img2');
+        break;
+        case 'img2':
+          this.set('actualClass', 'img3');
+        break;
+        case 'img3':
+          this.set('actualClass', 'img1');
+        break;
+      }
+      this.actualize();
+    }, 5000)
+  },
+
+  didInsertElement:function(){
+    console.log('TRUC')
+    this.actualize();
   }
 });
--- app/views/index.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.View.extend({
-  didInsertElement: function() {
-    console.log('init orbit')
-    Ember.$('#featured').orbit(
-    );
-  }
-});
